835bb8ca12dae5d4d9c3e14b566817bb143434b1
casca <8927157+casca@users.noreply.github.com>
2020-07-24 17:19:03 +0200

    refactor webpack configs and package.json scripts

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
 
-module.exports = {
+const config = {
   // inform webpack that we're building a bundle for Node.js, rather than for the browser
   target: 'node',
 
@@ -9,18 +11,6 @@ module.exports = {
 
   // tell webpack where to put the output file that is generated
   output: { filename: 'bundle.js', path: path.resolve(__dirname, 'build') },
-
-  // tell webpack to run babel on every file it runs through
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: ['react', 'stage-0', ['env', { target: { browsers: ['last 2 versions'] } }]],
-        },
-      },
-    ],
-  },
 };
+
+module.exports = merge(baseConfig, config);
