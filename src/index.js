6fbfb554c07a8e24c8295364fe994c819e7eed5c
casca <8927157+casca@users.noreply.github.com>
2020-07-27 21:02:28 +0200

    create redux store client and server side

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@
 // const Home = require('./client/components/Home').default;
 
 // isomorphic
+import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 const app = express();
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  res.send(renderer(req));
+  const store = createStore();
+  res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {
