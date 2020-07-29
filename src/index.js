814608fef925e0af04904a05fdd6ae70e158cd44
casca <8927157+casca@users.noreply.github.com>
2020-07-29 20:54:26 +0200

    load data and use it when rendering server-side

--- src/index.js
+++ src/index.js
@@ -8,13 +8,25 @@ import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
+import { matchRoutes } from 'react-router-config';
+import Routes from './client/Routes';
 
 const app = express();
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
   const store = createStore();
-  res.send(renderer(req, store));
+
+  // matchRoutes(Routes, req.path).map(({ route }) => {
+  //   return route.loadData ? route.loadData() : null;
+  // });
+  const promises = matchRoutes(Routes, req.path).map(({ route }) =>
+    route.loadData ? route.loadData(store) : null,
+  );
+
+  Promise.all(promises).then(() => {
+    res.send(renderer(req, store));
+  });
 });
 
 app.listen(3000, () => {
