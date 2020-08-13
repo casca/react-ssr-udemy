0fe469b0569a7d8c292972f2b1c2d7ff0f4e63d9
casca <8927157+casca@users.noreply.github.com>
2020-08-13 18:43:37 +0200

    add meta tags with react-helmet

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom';
-import Routes from '../client/Routes';
 import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
+import { StaticRouter } from 'react-router-dom';
 import serialize from 'serialize-javascript';
+import Routes from '../client/Routes';
+import { Helmet } from 'react-helmet';
 
 export default (req, store, context) => {
   const content = renderToString(
@@ -15,9 +16,13 @@ export default (req, store, context) => {
     </Provider>,
   );
 
+  const helmet = Helmet.renderStatic();
+
   return `
     <html>
       <head>
+        ${helmet.title.toString()}
+        ${helmet.meta.toString()}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
       </head>
       <body>
